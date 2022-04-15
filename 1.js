1a147f8dce069b399a5b740ad8c4d271a23319ea
tuanletk1 <tuanletk1@gmail.com>
2022-04-15 12:00:08 +0700

    Update

--- 1.js
+++
@@ -1,6 +0,0 @@
-var $ = document.querySelector.bind(document)
-$(".sub-menu ul").hidden();
-$(".sub-menu a").click(function () {
-  $(this).parent(".sub-menu").children("ul").slideToggle("200");
-  $(this).find("i.fa").toggleClass("fa-angle-up fa-angle-down");
-});
