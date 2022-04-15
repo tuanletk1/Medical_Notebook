ff86472b483dff7a6f54063a903a84e65c6e1451
Tran Dinh Viet Tuong <trandinhviettuong2000@gmail.com>
2022-04-15 10:56:17 +0700

    Init migration

---
+++ api/database/migrations/2022_04_16_054935_create_symptoms_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('symptoms', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('vacc_oder_register_id');
+            $table->foreign('vacc_oder_register_id')->references('id')->on('vacc_oder_register');
+            $table->string('descriptions', 255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('symptoms');
+    }
+};
